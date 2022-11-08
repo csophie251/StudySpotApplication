29e47f4b1c33ce7fe28ebcddd8c60c0cb065fbfb
Sophie Cisse <cisse@usc.edu>
2022-11-08 00:53:54 -0800

    layout for studyspotactivities

--- app/src/main/java/com/example/studyspotapplication/StudySpotAuthenticatedActivity.java
+++ app/src/main/java/com/example/studyspotapplication/StudySpotAuthenticatedActivity.java
@@ -3,16 +3,23 @@ package com.example.studyspotapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RatingBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudySpotAuthenticatedActivity extends AppCompatActivity {
-        @Override
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_study_spot_authenticated);
-        }
+            final RatingBar ratingBar = findViewById(R.id.rating);
+            String rating = "Rating is :" + ratingBar.getRating();
 
+        }
+        public void goToLoginPage(android.view.View view) {
+            Intent intent = new Intent(this, LoginPageActivity.class);
+            startActivity(intent);
+        }
         public void rateLocation(android.view.View view){
             Intent intent = new Intent (this, LoginPageActivity.class);
             startActivity(intent);
