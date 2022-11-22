8d9bcf744bce3316264b03349b267854562e7728
Steve Vott <svott@usc.edu>
2022-11-21 21:39:04 -0800

    Util

---
+++ app/src/main/java/com/example/studyspotapplication/Util.java
@@ -0,0 +1,14 @@
+package com.example.studyspotapplication;
+
+import java.util.ArrayList;
+
+public class Util {
+    public ArrayList<StudySpot> retrieveAllPages() {
+        // winston code!
+    }
+
+    public StudySpot retrievePage(String pageNam) {
+        // winston code!
+        return new StudySpot();
+    }
+}
